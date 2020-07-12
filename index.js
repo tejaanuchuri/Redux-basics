965202935fe98669bd82616906c7b8b79a69a60c
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-12 13:26:36 +0530

    "state update"

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 function createStore () {
     let state // create a state variable is going to change to hold entire application
-    
+    let listeners = []
     // acess the state
      const getState = () => state;
-
+     const subscribe = (listener) => {
+         listeners.push(listener)
+         return () => {
+             listeners = listeners.filter((l)=>l!==listener)
+         }
+     }
      return {
-        getState
+        getState,
+        subscribe
     }
 }
 //create store variable 
@@ -15,5 +21,13 @@ const store = createStore()
 store.subscribe(()=>{
     console.log('the new state is :', store.getState());
 })
+
+//update the state
+const unsubscribe = store.subscribe(()=>{
+    console.log('the state change');
+})
+
+unsubscribe()
+
 
 
