c6705afb00465082b7e3cfecdf0d299e3583dac7
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-12 15:06:30 +0530

    " ad goals to our todolist"

--- index.js
+++ index.js
@@ -26,14 +26,21 @@ function createStore (reducer) {
 // App code
 
 function todos (state = [],action) {  //reducer function
-    if (action.type == 'ADD_TODO') {
-        return state.concat([action.todo])
-    }else if(action.type === 'REMOVE_TODO'){
-        return state.filter((todo)=> todo.id !== action.id)
+    switch(action.type){
+        case  'ADD_TODO': return state.concat([action.todo])
+        case 'REMOVE_TODO': return state.filter((todo)=> todo.id !== action.id) 
+        case 'TOGGLE_TODO': return state.map((todo) => todo.id !== action.id?todo:Object.assign({},todo,{complete: !todo.complete})) 
+        default: return state
     }
-    return state
 }
 
+function goals (state = [],action){
+    switch(action.type){
+        case  'ADD_TODO': return state.concat([action.goal])
+        case 'REMOVE_TODO': return state.filter((goal)=> goal.id !== action.id) 
+        default: return state
+    }
+}
 
 //create store variable 
 const store = createStore(todos)
