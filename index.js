2a06e0d929b26e6e71d7529572988572c19e5004
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-12 12:49:12 +0530

    " acess the state"

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function createStore () {
     let state // create a state variable is going to change to hold entire application
+    
+    // acess the state
+     const getState = () => state;
+
+     
 }
