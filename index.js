195a2bdd49254d7be9f790b9def046145352b640
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-12 18:36:19 +0530

    "Add todo and goal in the ui "

--- index.js
+++ index.js
@@ -25,29 +25,39 @@ function createStore (reducer) {
 
 // App code
 
-function todos (state = [],action) {  //reducer function
-    switch(action.type){
-        case  'ADD_TODO': return state.concat([action.todo])
-        case 'REMOVE_TODO': return state.filter((todo)=> todo.id !== action.id) 
-        case 'TOGGLE_TODO': return state.map((todo) => todo.id !== action.id?todo:Object.assign({},todo,{complete: !todo.complete})) 
-        default: return state
+function todos (state = [], action) {
+    switch(action.type) {
+      case 'ADD_TODO' :
+        return state.concat([action.todo])
+      case 'REMOVE_TODO' :
+        return state.filter((todo) => todo.id !== action.id)
+      case 'TOGGLE_TODO' :
+        return state.map((todo) => todo.id !== action.id ? todo :
+          Object.assign({}, todo, { complete: !todo.complete }))
+      default :
+        return state
     }
-}
-
-function goals (state = [],action){
-    switch(action.type){
-        case  'ADD_TODO': return state.concat([action.goal])
-        case 'REMOVE_TODO': return state.filter((goal)=> goal.id !== action.id) 
-        default: return state
+  }
+  
+  function goals (state = [], action) {
+    switch(action.type) {
+      case 'ADD_GOAL' :
+        return state.concat([action.goal])
+      case 'REMOVE_GOAL' :
+        return state.filter((goal) => goal.id !== action.id)
+      default :
+        return state
     }
-}
-
-function app (state={}, action) {
+  }
+  
+  function app (state = {}, action) {
     return {
-        todo: {state.todos,action},
-        goals: {state.goal,action},
+      todos: todos(state.todos, action),
+      goals: goals(state.goals, action),
     }
-}
+  }
+  
+  
 //create store variable 
 const store = createStore(app)
 
